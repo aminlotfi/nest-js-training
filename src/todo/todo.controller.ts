d141c4dd0aa0192ef2ec756c84afe86f0bbc0c83
Mohammad Amin Lotfi <a.lotfi1201@gmail.com>
2023-09-14 21:25:25 +0330

    define methods and route endpoints in TodoController

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Post, Put} from '@nestjs/common';
+import {TodoService} from "./todo.service";
+import {Todo} from "./schemas/todo.schema";
+import {CreateTodoDto} from "./dto/create-todo.dto";
+import {UpdateTodoDto} from "./dto/update-todo.dto";
 
 @Controller('todo')
-export class TodoController {}
+export class TodoController {
+    constructor(private readonly service: TodoService) {
+    }
+
+    @Get()
+    async findAll(): Promise<Todo[]> {
+        return await this.service.findAll();
+    }
+
+    @Get(':id')
+    async findOne(id: string): Promise<Todo> {
+        return await this.service.findOne(id);
+    }
+
+    @Post()
+    async create(@Body() createTodoDto: CreateTodoDto): Promise<Todo> {
+        return await this.service.create(createTodoDto);
+    }
+
+    @Put(':id')
+    async update(@Param('id') id: string, @Body() updateTodoDto: UpdateTodoDto): Promise<Todo> {
+        return await this.service.update(id, updateTodoDto);
+    }
+
+    @Delete(':id')
+    async remove(@Param('id') id: string): Promise<Todo> {
+        return await this.service.remove(id);
+    }
+}
